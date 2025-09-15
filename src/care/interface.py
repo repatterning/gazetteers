d5304b847c08b7929e17eff217092afd8c285ef0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-15 15:50:27 +0100

    Update src/care/interface.py

--- src/care/interface.py
+++ src/care/interface.py
@@ -1,20 +1,18 @@
 """Module algorithms/interface.py"""
-import io
 import logging
-import sys
-import xml.etree.ElementTree as et
+import os
 
 import boto3
 import geopandas
-import requests
 
+import config
 import src.functions.cache
 import src.functions.secret
 
 
 class Interface:
     """
-    The interface to the programs of the algorithms package.
+    The interface to the programs of the care package.
     """
 
     def __init__(self, connector: boto3.session.Session, arguments: dict):
@@ -29,3 +27,30 @@ class Interface:
         self.__arguments = arguments
 
         self.__secret = src.functions.secret.Secret(connector=self.__connector)
+        self.__configurations = config.Config()
+
+    def __get_data(self) -> geopandas.GeoDataFrame:
+        """
+        Retrieves a gazetteer of Scotland's care homes.
+
+        :return:
+        """
+
+        authkey = self.__secret.exc(secret_id=self.__arguments.get('project_key_name'), node='spatial-hub-geoserver')
+        filename = self.__configurations.care_.format(authkey=authkey)
+
+        try:
+            return geopandas.read_file(filename=filename)
+        except FileNotFoundError as err:
+            raise err from err
+
+    def exc(self):
+
+        frame = self.__get_data()
+        filename = os.path.join(self.__configurations.cartography_, 'care.geojson')
+
+        try:
+            frame.to_file(filename=filename, driver='GeoJSON')
+            logging.info('%s: Succeeded', filename)
+        except RuntimeError as err:
+            raise err from err
