1900e2e21b9a8aabf223be0ae779da6442094ef2
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-15 22:41:05 +0100

    Add src/script.py

---
+++ src/script.py
@@ -0,0 +1,37 @@
+"""Module script.py"""
+import datetime
+import logging
+import os
+import sys
+
+
+def main():
+    """
+
+    :return:
+    """
+
+    logger: logging.Logger = logging.getLogger(__name__)
+    logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
+
+    # Rivers
+    src.rivers.interface.Interface().exc()
+
+    # Deleting __pycache__
+    src.functions.cache.Cache().exc()
+
+if __name__ == '__main__':
+
+    # Setting-up
+    root = os.getcwd()
+    sys.path.append(root)
+    sys.path.append(os.path.join(root, 'src'))
+
+    logging.basicConfig(level=logging.INFO,
+                        format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    import src.rivers.interface
+    import src.functions.cache
+
+    main()
