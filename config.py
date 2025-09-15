d448ada83a05d2ab8b1eb6bae382bae36e6d4408
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-15 15:54:26 +0100

    Update config.py

--- config.py
+++ config.py
@@ -19,13 +19,14 @@ class Config:
         """
 
         self.warehouse: str = os.path.join(os.getcwd(), 'warehouse')
-        self.series_ = os.path.join(self.warehouse, 'data', 'series')
         self.references_ = os.path.join(self.warehouse, 'references')
+        self.cartography_ = os.path.join(self.warehouse, 'cartography')
 
-        # Template
+        # Keys
         self.s3_parameters_key = 's3_parameters.yaml'
+        self.arguments_key = 'gazetteers/arguments.json'
 
         # Care
         self.care_ = ('https://geo.spatialhub.scot/geoserver/sh_chep/wfs?service=WFS&'
-                      'authkey={spatial-hub-geoserver}&request=GetFeature&typeName=sh_chep:pub_chep&format_options'
+                      'authkey={authkey}&request=GetFeature&typeName=sh_chep:pub_chep&format_options'
                       '=filename:Care_Homes_for_Older_People_-_Scotland&outputFormat=application/json')
