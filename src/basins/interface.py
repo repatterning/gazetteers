570af7edb74926c04328b795d109ab69f29c5958
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-16 20:10:52 +0100

    Update src/basins/interface.py

--- src/basins/interface.py
+++ src/basins/interface.py
@@ -1,9 +1,11 @@
 """Module rivers/interface.py"""
 import logging
+import os
 
 import geopandas
 import pandas as pd
 
+import config
 import src.basins.coarse
 import src.basins.fine
 
@@ -18,11 +20,28 @@ class Interface:
         Constructor
         """
 
+        self.__configurations = config.Config()
+
         # Logging
         logging.basicConfig(level=logging.INFO,
                             format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
+
+    def __persist(self, coarse: geopandas.GeoDataFrame):
+        """
+
+        :param coarse:
+        :return:
+        """
+
+        filename = os.path.join(self.__configurations.cartography_, 'coarse.geojson')
+
+        try:
+            coarse.to_file(filename=filename, driver='GeoJSON')
+            logging.info('%s: Succeeded', filename)
+        except RuntimeError as err:
+            raise err from err
 
     def exc(self, assets: pd.DataFrame):
         """
@@ -36,3 +55,5 @@ class Interface:
 
         coarse = src.basins.coarse.Coarse(assets=assets, fine=fine).exc()
         self.__logger.info(coarse)
+
+        self.__persist(coarse=coarse)
