ed1698a15deec43309046765fee1d56c643a455d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-17 14:38:50 +0100

    Update src/script.py

--- src/script.py
+++ src/script.py
@@ -3,6 +3,7 @@ import datetime
 import logging
 import os
 import sys
+import geopandas
 
 
 def main():
@@ -14,10 +15,20 @@ def main():
     logger: logging.Logger = logging.getLogger(__name__)
     logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
 
+    # Paths
     strings = src.transfer.dictionary.Dictionary().exc(
         path=os.path.join(os.getcwd(), 'warehouse'), extension='*', prefix='')
     strings['metadata'] = strings['vertex'].apply(lambda x: metadata[os.path.basename(x)])
     logger.info(strings)
+
+    # Exploring geopandas & zip
+    path = os.path.join(configurations.data, 'cartography', 'SEPA.zip')
+    frame = geopandas.read_file(f'zip:{os.sep}{os.sep}{path}')
+    logger.info(frame)
+
+    frame.to_file(
+        filename=os.path.join(configurations.cartography_, 'SEPA.geojson'),
+        driver='GeoJSON')
 
     # Deleting __pycache__
     src.functions.cache.Cache().exc()
@@ -33,10 +44,12 @@ if __name__ == '__main__':
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
+    import config
     import src.functions.cache
     import src.transfer.dictionary
     import src.metadata.interface
 
+    configurations = config.Config()
     metadata = src.metadata.interface.Interface().metadata
 
     main()
