4e3024e50435a477f4367cb2299a7db3058dea51
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-09-17 11:35:22 +0100

    src/script.py: Testing metadata settings.

--- src/script.py
+++ src/script.py
@@ -14,8 +14,10 @@ def main():
     logger: logging.Logger = logging.getLogger(__name__)
     logger.info('Starting: %s', datetime.datetime.now().isoformat(timespec='microseconds'))
 
-    # Basins
-    src.basins.interface.Interface().exc()
+    strings = src.transfer.dictionary.Dictionary().exc(
+        path=os.path.join(os.getcwd(), 'warehouse'), extension='*', prefix='')
+    strings['metadata'] = strings['vertex'].apply(lambda x: metadata[os.path.basename(x)])
+    logger.info(strings)
 
     # Deleting __pycache__
     src.functions.cache.Cache().exc()
@@ -31,7 +33,10 @@ if __name__ == '__main__':
                         format='\n\n%(message)s\n%(asctime)s.%(msecs)03d',
                         datefmt='%Y-%m-%d %H:%M:%S')
 
-    import src.basins.interface
     import src.functions.cache
+    import src.transfer.dictionary
+    import src.metadata.interface
+
+    metadata = src.metadata.interface.Interface().metadata
 
     main()
