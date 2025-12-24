f2f6f8d78fd048580b8f831740b310f37fd54734
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-24 23:38:36 +0000

    Add src/schools/interface.py

---
+++ src/schools/interface.py
@@ -0,0 +1,22 @@
+
+import boto3
+
+import src.schools.data
+
+
+class Interface:
+
+    def __init__(self, connector: boto3.session.Session, arguments: dict):
+        """
+
+        :param connector: A boto3 session instance, it retrieves the developer's <default> Amazon
+                          Web Services (AWS) profile details, which allows for programmatic interaction with AWS.
+        :param arguments: A set of arguments vis-à-vis computation & data operations objectives.
+        """
+
+        self.__connector = connector
+        self.__arguments = arguments
+
+    def exc(self):
+
+        src.schools.data.Data(connector=self.__connector, arguments=self.__arguments).exc()
