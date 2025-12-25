305f053352d4cd10cedae87391da00069b834c8f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-25 23:42:28 +0000

    Update src/gauges/codes.py

--- src/gauges/codes.py
+++ src/gauges/codes.py
@@ -1,8 +1,13 @@
 """Module codes.py"""
+
+import logging
+
 import pandas as pd
+import boto3
 
 import src.elements.text_attributes as txa
 import src.functions.streams
+import src.gauges.special
 
 
 class Codes:
@@ -13,10 +18,14 @@ class Codes:
     Extracts time series codes.<br>
     """
 
-    def __init__(self):
+    def __init__(self, connector: boto3.session.Session):
         """
-        Construction
+
+        :param connector: A boto3 session instance, it retrieves the developer's <default> Amazon
+                          Web Services (AWS) profile details, which allows for programmatic interaction with AWS.
         """
+
+        self.__connector = connector
 
         self.__streams = src.functions.streams.Streams()
 
@@ -33,8 +42,31 @@ class Codes:
         :return:
         """
 
-        text = txa.TextAttributes(uri=self.__uri, header=0, sep=';')
-        frame = self.__streams.api(text=text)
+        buffer = src.gauges.special.Special(connector=self.__connector).get_text(url=self.__uri)
+
+        text = txa.TextAttributes(uri=buffer, header=0, sep=';')
+        frame = self.__streams.read(text=text)
         frame.info()
+        logging.info(frame)
 
         return frame
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
