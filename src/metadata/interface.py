98fa00319d7b9d95eef6816f607b01db8b53e2fe
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-25 23:56:30 +0000

    Update src/metadata/interface.py

--- src/metadata/interface.py
+++ src/metadata/interface.py
@@ -33,19 +33,19 @@ class Interface:
                 "code": "The letter code of a measure.",
                 "description": "The description of the key and code."
             },
-            "sch-catchments": {
+            "sch-catchments.geojson": {
                 "desc": "The union of the four schools data.  Additionally, the data set includes a field indicating the catchment within which each school lies."
             },
-            "sch-primary-denominational": {
+            "sch-primary-denominational.geojson": {
                 "desc": "The denominational primary schools."
             },
-            "sch-primary-non-denominational": {
+            "sch-primary-non-denominational.geojson": {
                 "desc": "The non-denominational primary schools."
             },
-            "sch-secondary-denominational": {
+            "sch-secondary-denominational.geojson": {
                 "desc": "The denominational secondary schools."
             },
-            "sch-secondary-non-denominational": {
+            "sch-secondary-non-denominational.geojson": {
                 "desc": "The non-denominational secondary schools."
             },
             "assets.csv": {
