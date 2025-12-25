5cda9c73fd1922c92233f5eaa232a8b19abfc4b7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-25 17:06:50 +0000

    Update src/schools/interface.py

--- src/schools/interface.py
+++ src/schools/interface.py
@@ -1,9 +1,13 @@
 """Module interface.py"""
 import logging
+import os
 
 import boto3
 import geopandas
 
+import config
+import src.cuttings
+import src.membership
 import src.schools.data
 
 
@@ -12,16 +16,21 @@ class Interface:
     An interface to the `src.schools` package's programs
     """
 
-    def __init__(self, connector: boto3.session.Session, arguments: dict):
+    def __init__(self, connector: boto3.session.Session, coarse: geopandas.GeoDataFrame, arguments: dict):
         """
 
         :param connector: A boto3 session instance, it retrieves the developer's <default> Amazon
                           Web Services (AWS) profile details, which allows for programmatic interaction with AWS.
+        :param coarse: The coarse level river basins
         :param arguments: A set of arguments vis-à-vis computation & data operations objectives.
         """
 
         self.__connector = connector
+        self.__coarse = coarse
         self.__arguments = arguments
+
+        # Instances
+        self.__configurations = config.Config()
 
     def exc(self):
         """
@@ -29,5 +38,9 @@ class Interface:
         :return:
         """
 
-        data: geopandas.GeoDataFrame = src.schools.data.Data(connector=self.__connector, arguments=self.__arguments).exc()
-        logging.info(data)
+        data: geopandas.GeoDataFrame = src.schools.data.Data(
+            connector=self.__connector, arguments=self.__arguments).exc()
+
+        # Intersects, Persist
+        filename = os.path.join(self.__configurations.cartography_, 'sch-catchments.geojson')
+        src.membership.Membership(coarse=self.__coarse).exc(frame=data, filename=filename)
