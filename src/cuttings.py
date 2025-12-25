51d356c426bf87d2d74a277d64a6fc3fa532282f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-25 12:55:37 +0000

    Update src/cuttings.py

--- src/cuttings.py
+++ src/cuttings.py
@@ -6,26 +6,27 @@ import shapely
 
 class Cuttings:
     """
-    Herein, the aim is to determine the hydrometric catchment that a point belongs to.
+    Herein, the aim is to determine the hydrometric catchment that a point belongs to.  The catchments 
+    are contiguous, non-overlapping, catchments.  Each point should be a member of a single catchment only.
     """
 
-    def __init__(self, reference: geopandas.GeoDataFrame):
+    def __init__(self, places: geopandas.GeoDataFrame):
         """
 
-        :param reference: Each instance encodes the details of a care home.
+        :param places: Each instance encodes the details of a place.
         """
 
-        self.__reference = reference
+        self.__places = places
 
     def __is_member(self, _polygon: shapely.geometry.polygon.Polygon) -> pd.Series:
         """
-        Determines whether a home lies within a polygon; per home.
+        Determines whether each place lies within the input polygon.
 
         :param _polygon: The polygon of a catchment area
         :return:
         """
 
-        return self.__reference.geometry.apply(lambda y: y.within(_polygon))
+        return self.__places.geometry.apply(lambda y: y.within(_polygon))
 
     def members(self, _elements) -> geopandas.GeoDataFrame:
         """
@@ -35,9 +36,9 @@ class Cuttings:
         :return:
         """
 
-        states = self.__is_member(_polygon=_elements.geometry)
+        is_member_of: pd.Series = self.__is_member(_polygon=_elements.geometry)
 
-        frame = self.__reference.copy().loc[states, :]
+        frame = self.__places.copy().loc[is_member_of, :]
         frame['catchment_id'] = _elements.catchment_id
         frame['catchment_name'] = _elements.catchment_name
 
