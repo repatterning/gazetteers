627a3d0b5465e66569784abe6304a9d6c3f41e81
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-12-26 00:18:22 +0000

    Update src/gauges/codes.py

--- src/gauges/codes.py
+++ src/gauges/codes.py
@@ -1,9 +1,9 @@
 """Module codes.py"""
 
-import logging
+import io
 
-import pandas as pd
 import boto3
+import pandas as pd
 
 import src.elements.text_attributes as txa
 import src.functions.streams
@@ -27,9 +27,7 @@ class Codes:
 
         self.__connector = connector
 
-        self.__streams = src.functions.streams.Streams()
-
-        # # The uniform resource locator for the time series codes list vis-à-vis <Level>
+        # The uniform resource locator for the time series codes list vis-à-vis <Level>
         self.__uri = ('https://timeseries.sepa.org.uk/KiWIS/KiWIS?service=kisters&type=queryServices&datasource=0'
                       '&request=getTimeseriesList&catchment_no=*&stationparameter_name=Level&ts_name=15minute'
                       '&returnfields=catchment_id,catchment_no,catchment_name,station_id,station_no,station_name,'
@@ -42,31 +40,8 @@ class Codes:
         :return:
         """
 
-        buffer = src.gauges.special.Special(connector=self.__connector).get_text(url=self.__uri)
-
+        buffer: io.StringIO = src.gauges.special.Special(connector=self.__connector).get_text(url=self.__uri)
         text = txa.TextAttributes(uri=buffer, header=0, sep=';')
-        frame = self.__streams.read(text=text)
-        frame.info()
-        logging.info(frame)
+        frame = src.functions.streams.Streams().read(text=text)
 
         return frame
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
